a8f13fb2cc9a4e908bd835a4cee02804b978e535
Jonathan Reibel <reibelj@aldenv173.allegheny.edu>
2017-10-18 19:10:21 -0400

    Adding the files for lab 6

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -3,13 +3,21 @@ package labsix;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * This class will manipulate strings of DNA.
+ * The program will read in an input and display a complement or mutation based on DNA
+ */
 public class ManipulateDna {
+  /**
+   * This is the main method of ManipulateDNA.
+   */
 
   public static void main(String[] args) {
     // display the name of the programmer and the date
-    System.out.println("Gregory M. Kapfhammer " + new Date());
+    System.out.println("Jonathan Reibel " + new Date());
     // declare the starting file and scanner
     File dnaInputsFile = null;
     Scanner scanner = null;
@@ -21,17 +29,35 @@ public class ManipulateDna {
       System.out.println("Unable to locate file");
     }
     // Step One: Read in and display the chosen DNA string
-
+    String dna = scanner.nextLine();
+    System.out.println("Okay, I am going to manipulate the DNA string" + dna + ".");
+    System.out.println("");
     // Step Two: Compute the complement of the DNA String
-
+    String updna = dna.toUpperCase();
+    System.out.println("Enter a string Containing only C, G, T, and A:" + dna);
+    String comp1 = dna.replace('a','T');
+    String comp2 = comp1.replace('t','A');
+    String comp3 = comp2.replace('g','C');
+    String comp4 = comp3.replace('c','G');
+    System.out.println("The complement of" + updna + "is" + comp4);
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
-
+    Random rnd = new Random();
+    int into = rnd.nextInt(updna.length());
+    String dnastring = "ACTG";
+    char letter = dnastring.charAt(rnd.nextInt(dnastring.length()));
+    String insert = updna.substring(0, into) + letter + updna.substring(into); 
+    System.out.println("Inserting " + letter + " into positon " + into + " gives " + insert);
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
-
+    int outof = rnd.nextInt(updna.length());
+    String remove = updna.substring(0, outof) + updna.substring(outof + 1);
+    System.out.println("Removing from postion " + outof + " gives " + remove);
     // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
-
+    int change = rnd.nextInt(updna.length());
+    String changing = updna.substring(0, change) + letter + updna.substring(change + 1); 
+    System.out.println("Changing position " + change + " gives " + changing);
     // Step Six: Display a final thankyou message
-
+    System.out.println("Thank you for using the Manipulate DNA program.");
+    
   }
 
 }
--- writing/reflection.md
+++ writing/reflection.md
@@ -1,4 +1,23 @@
 # Reflection by
 
-This lab required me to write a file called `ManipulateDNA`. Please review the
-Laboratory Assignment Sheet and add the required details to this paragraph.
+This lab required me to complete the code for a file called Manipulate DNA.
+This was a little difficult for me. It took into account many of the methods
+that have been used and combined them all. My biggest problem was just trying
+to picture how the system would accomplish the task. For example, in step three
+four and five I was required to manipulate the string of DNA. I had to stop and
+think of how I was supposed to break up the string and combine the substrings
+back in order to remove add or change the string. This was the most challenging
+part for me.
+
+Some other problems I ran into included the use of the random Java class. I
+tried calling that method in order to choose the random position within the
+chosen DNA string. However, it was not imported at the top of the source code
+which was caused a little problem. Once that was resolved I was able to move
+forward with more ease.
+
+As previously mentioned this lab required calling back a lot of information
+from previous labs. Once I was able to realize that I had to make use of these
+methods I was able to start making progress. Overall, it was a difficult lab
+because I had to think outside of the source code so to speak. Once I saw how
+the strings needed to be changed I was able to use the methods learned in order
+to correctly achieve the goal and complete the assignment.
